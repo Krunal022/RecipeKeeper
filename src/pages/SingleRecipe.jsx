95e8fe977eb8f43c7c0a61a534fae2200d9c21b3
Krunal022 <krunalvaghela723@gmail.com>
2025-06-14 23:02:13 +0530

    recipe details on detail page!

--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { recipecontext } from "../context/RecipeContext";
+import { useNavigate, useParams } from "react-router-dom";
 
 const SingleRecipe = () => {
-  return (
-    <div>SingleRecipe</div>
-  )
-}
+  const { data, setdata } = useContext(recipecontext);
+   const navigate = useNavigate();
+    const params = useParams();
+    const recipe = data.find((recipe) => params.id == recipe.id);
 
-export default SingleRecipe
+    return recipe ? (
+        <div className="w-200 flex">
+            <div className="left w-1/2 p-2">
+                <h1 className="text-3xl font-black">{recipe.title}</h1>
+                <img className="h-[20vh]" src={recipe.image} alt="" />
+                <h1>{recipe.chef}</h1>
+                <p>{recipe.desc}</p>
+            </div>
+        </div>
+    ) : (
+        <div>No recipe found!</div>
+    );
+};
+
+export default SingleRecipe;
